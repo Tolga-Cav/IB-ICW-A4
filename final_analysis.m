70011da7a82068ea42cb48beb69db7e1d444caa4
Tolga <tc704@cam.ac.uk>
2026-03-15 19:20:27 +0000

    Added comments to make code clearer

--- final_analysis.m
+++ final_analysis.m
@@ -15,12 +15,12 @@ window_names = ["Rectangular","Hamming", "Hanning", "Kaiser", "Bartlett"];
 overlaps = [0.0 , 0.25, 0.50, 0.75, 0.9];
 
 %List of data files
-%files = ["lab_data/sin_random.mat"]; %["lab_data/sin_10.mat", "lab_data/sin_9_13.mat", "lab_data/sin_3_9_13.mat", "lab_data/sin_random.mat"];
+%files = ["lab_data/sin_10.mat", "lab_data/sin_9_13.mat", "lab_data/sin_3_9_13.mat", "lab_data/sin_random.mat"];
 
 %Earthquake filenames
 files = ["earthquake_data/dataset1-small.mat", "earthquake_data/dataset2-moderate.mat", "earthquake_data/dataset3-large.mat"];
 
-% plot_names =["Three Sins Input"];% ["One Sin Input","Two Sins Input","Three Sins Input", "Random Input"];
+% plot_names =["One Sin Input","Two Sins Input","Three Sins Input", "Random Input"];
 plot_names = ["Small Earthquake", "Moderate Earthquake", "Large Earthquake"];
 
 %Loop this for each input we gave it
@@ -57,7 +57,6 @@ for file_index = 1:length(files)
             transforms = [];
 
             % % Model
-            % % For speed if needed, make these empty arrays the size of max k
             % transforms_1 = [];
             % transforms_2 = [];
 
@@ -107,31 +106,31 @@ for file_index = 1:length(files)
                 % transforms_2 = [transforms_2,abs(f_ch2)];
 
                 
-                % %Plotting individual transforms
-                % if k==5 & overlap==0
-                %     figure
-                %     hold on
-                %     plot(x,abs(f_ch2));
-                %     title('Transform of Channel 2 with' + " a " + window_name + " Window")
-                %     V = axis;
-                %     axis([0 30 V(3) V(4)])
-                %     xlabel('Frequency (Hz)');
-                %     ylabel('Amplitude (V/Hz)');
-                %     hold off
-                %     saveas(gcf, "transform"+window_name+".png");
-                % end
-                % 
-                % %Plotting the window functions
-                % if k==5 & overlap==0
-                %     figure
-                %     hold on
-                %     plot(window);
-                %     title("A " + window_name + " Window")
-                %     xlabel("Sample #");
-                %     ylabel("Scale factor")
-                %     hold off
-                %     saveas(gcf, window_name+".png");
-                % end                
+                %Plotting individual transforms
+                if k==5 & overlap==0
+                    figure
+                    hold on
+                    plot(x,abs(f_ch2));
+                    title('Transform of Channel 2 with' + " a " + window_name + " Window")
+                    V = axis;
+                    axis([0 30 V(3) V(4)])
+                    xlabel('Frequency (Hz)');
+                    ylabel('Amplitude (V/Hz)');
+                    hold off
+                    saveas(gcf, "transform"+window_name+".png");
+                end
+
+                %Plotting the window functions
+                if k==5 & overlap==0
+                    figure
+                    hold on
+                    plot(window);
+                    title("A " + window_name + " Window")
+                    xlabel("Sample #");
+                    ylabel("Scale factor")
+                    hold off
+                    saveas(gcf, window_name+".png");
+                end                
             end
             figure
             hold on 
@@ -159,7 +158,6 @@ for file_index = 1:length(files)
             % grid;
             % xlabel('Frequency'); ylabel('Window #'); zlabel('');
             % view(57,44); %Adjust viewing angle so you can clearly see data
-            % % CHANGE THIS
 
             hold off
             if window_name == "Hamming"
